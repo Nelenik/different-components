723c6bef99283e133a7ced12ac345cac0b7fb8b0
Nelenik <100496821+Nelenik@users.noreply.github.com>
2023-04-29 17:27:35 +0300

    the plugin has been rewritten with classes, target el and movable blocks are now passed as an array

--- move-block/move-block.js
+++ move-block/move-block.js
@@ -1,99 +1,101 @@
-function MoveBlock(options) {
-    const core = {
-      init() {
-        this.block = this.isString(options.singleBlock) ? document.querySelector(options.singleBlock) : options.singleBlock;
-        this.breakpoints = options.breakpoints;
-        this.mediaQuery;
-        this.setMedia()
-      },
-      setMedia() {
-        this.breakpoints.forEach(point => {
-          const { solution } = point;
-          let mediaQuery = window.matchMedia(`${solution}`);
-          if(mediaQuery.matches) this.checkMethod(point)
-          mediaQuery.addEventListener('change', function(e) {
-            if(e.matches) this.checkMethod(point)
-          }.bind(this))
-        })
-      },
-      checkMethod(point) {
-        const { method, target, elems, clearTarget } = point;
-        let elemToPaste = elems ? elems : [this.block] 
-        switch (method) {
-          case 'append':
-            target.append(...elemToPaste);
-            break;
-          case 'prepend':
-            target.prepend(...elemToPaste);
-            break;
-          case 'after':
-            target.after(...elemToPaste);
-            break;
-          case 'before':
-            target.before(...elemToPaste);
-            break;
-          default:
-            target.replaceWith(...elemToPaste);
-            break;
-  
-        }
-      },
-      isString(arg) {
-        return typeof arg === "string";
-      },
-    }
-    core.init()
+class MoveBlock {
+  constructor(breakpoints) {
+    this.breakpoints = breakpoints;
+    this.setMedia();
   }
 
+  setMedia() {
+    this.breakpoints.forEach(point => {
+      const { solution } = point;
+      let mediaQuery = window.matchMedia(`${solution}`);
+      if (mediaQuery.matches) this.moveBlock(point);
+      mediaQuery.addEventListener('change', (e) => {
+        if (e.matches) this.moveBlock(point)
+      })
+    })
+  }
+
+  moveBlock(point) {
+    const { targetEl, elToMove, insertMethod } = point;
+    const elToMoveArr = elToMove.map(item => this.isArray(item) ? item : [item]);
+    console.log(elToMoveArr)
+    targetEl.forEach((el, i) => {
+      const method = this.isString(insertMethod) ? insertMethod : insertMethod[i]
+      this.checkMethod(method, el, elToMoveArr[i])
+    })
+
+  }
+
+  checkMethod(method, target, elemToPaste) {
+    switch (method) {
+      case 'append':
+        target.append(...elemToPaste);
+        break;
+      case 'prepend':
+        target.prepend(...elemToPaste);
+        break;
+      case 'after':
+        target.after(...elemToPaste);
+        break;
+      case 'before':
+        target.before(...elemToPaste);
+        break;
+      default:
+        target.replaceWith(...elemToPaste);
+        break;
+
+    }
+  }
+
+  isArray(value) {
+    return Array.isArray(value)
+  }
+  isString(value) {
+    return typeof value === "string"
+  }
+}
+
+// Initialization, example
 
 const logo = document.querySelector('.logo')
 const submenu = document.querySelector('.submenu')
 const menu = document.querySelector('.menu')
-let variant = [logo, menu,submenu]
+let variant = [logo, menu, submenu]
 let variant1 = [logo, submenu, menu]
-let variant2= [menu, logo, submenu]
+let variant2 = [menu, logo, submenu]
 
 
-new MoveBlock({
-  breakpoints: [
+let block = new MoveBlock(
+  [
     {
       solution: "(min-width: 1000px)",
-      target: document.querySelector('.header__container'),
-      elems: variant,
-      method: 'append'
+      targetEl: [document.querySelector('.header__container')],
+      elToMove: [variant],
+      insertMethod: 'append'
     },
     {
       solution: "(min-width: 600px) and (max-width: 999px)",
-      target: document.querySelector('.header__container'),
-      elems: variant1,
-      method: 'append'
+      targetEl: [document.querySelector('.header__container')],
+      elToMove:[ variant1],
+      insertMethod: 'append'
     },
     {
       solution: "(max-width: 599px)",
-      target: document.querySelector('.header__container'),
-      elems: variant2,
-      method: 'append'
+      targetEl: [document.querySelector('.header__container')],
+      elToMove: [variant2],
+      insertMethod: 'append'
+    },
+    {
+      solution: "(min-width: 599px)",
+      targetEl: [document.querySelector('.menu')],
+      elToMove: [document.querySelector('.menu__item:last-child')],
+      insertMethod: 'append'
+    },
+    {
+      solution: "(max-width: 600px)",
+      targetEl: [document.querySelector('.submenu')],
+      elToMove: [document.querySelector('.menu__item:last-child')],
+      insertMethod: 'append'
     }
   ]
-})
-
-// new MoveBlock({
-//   singleBlock: '.logo',
-//   breakpoints: [
-//     {
-//       solution: "(min-width: 1000px)",
-//       target: document.querySelector('.menu'),
-//       method: 'before'
-//     },
-//     {
-//       solution: "(min-width: 600px) and (max-width: 999px)",
-//       target: document.querySelector('.menu'),
-//       method: 'after'
-//     },
-//     {
-//       solution: "(max-width: 599px)",
-//       target: document.querySelector('.submenu'),
-//       method: 'after'
-//     }
-//   ]
-// })
+)
